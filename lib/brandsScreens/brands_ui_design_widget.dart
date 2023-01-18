b64fafb4bb765159a2eee1f447152a08f20bf449
shodav4real <shonibaredavida@yahoo.com>
2023-01-18 04:18:25 +0100

    Completed

---
+++ lib/brandsScreens/brands_ui_design_widget.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:sellers_app/Models/brands_model.dart';
+
+class BrandsUIDesignWidget extends StatefulWidget {
+  Brands? model;
+  BuildContext? context;
+  BrandsUIDesignWidget({this.context, this.model});
+  @override
+  State<BrandsUIDesignWidget> createState() => _BrandsUIDesignWidgetState();
+}
+
+class _BrandsUIDesignWidgetState extends State<BrandsUIDesignWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      shadowColor: Colors.black,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: SizedBox(
+          // height: 70,
+          width: MediaQuery.of(context).size.width,
+          child: Column(children: [
+            Image.network(
+              widget.model!.thumbnailUrl.toString(),
+              height: 220,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 2,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  widget.model!.brandTitle.toString(),
+                  style: const TextStyle(
+                    color: Colors.deepPurple,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    letterSpacing: 3,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.delete_sweep),
+                  color: Colors.red,
+                )
+              ],
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
